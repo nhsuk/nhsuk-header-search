adfcd873e431442300fd860f609b61f3e30d9e7f
roshaanbajwa <roshaan.bajwa1@nhs.net>
2023-02-16 16:00:04 +0000

    whitespace

--- src/js/header-search.js
+++ src/js/header-search.js
@@ -24,7 +24,7 @@ import AccessibleAutoComplete from 'accessible-autocomplete';
     const dots = result.length > truncateLength ? '...' : '';
     const resultTruncated = result.substring(0, truncateLength) + dots;
     return `
-        <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
+      <svg class="nhsuk-icon nhsuk-icon__search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true"><path d="M19.71 18.29l-4.11-4.1a7 7 0 1 0-1.41 1.41l4.1 4.11a1 1 0 0 0 1.42 0 1 1 0 0 0 0-1.42zM5 10a5 5 0 1 1 5 5 5 5 0 0 1-5-5z"></path></svg>
         ${resultTruncated}
       `;
   };
